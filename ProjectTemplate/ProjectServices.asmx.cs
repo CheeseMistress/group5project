e3c3306e3f7559e62c5e84f9b609542f1a3d0d4d
Lindsey <lkoppy@asu.edu>
2022-02-06 18:13:51 -0700

    updated web method to return an array of users and passwords

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -9,54 +9,73 @@ using System.Data;
 
 namespace ProjectTemplate
 {
-	[WebService(Namespace = "http://tempuri.org/")]
-	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-	[System.ComponentModel.ToolboxItem(false)]
-	[System.Web.Script.Services.ScriptService]
-
-	public class ProjectServices : System.Web.Services.WebService
-	{
-		////////////////////////////////////////////////////////////////////////
-		///replace the values of these variables with your database credentials
-		////////////////////////////////////////////////////////////////////////
-		private string dbID = "spring2022team5";
-		private string dbPass = "spring2022team5";
-		private string dbName = "spring2022team5";
-		////////////////////////////////////////////////////////////////////////
-		
-		////////////////////////////////////////////////////////////////////////
-		///call this method anywhere that you need the connection string!
-		////////////////////////////////////////////////////////////////////////
-		private string getConString() {
-			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName+"; UID=" + dbID + "; PASSWORD=" + dbPass;
-		}
-		////////////////////////////////////////////////////////////////////////
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
 
 
+    public class ProjectServices : System.Web.Services.WebService
+    {
+        ////////////////////////////////////////////////////////////////////////
+        ///replace the values of these variables with your database credentials
+        ////////////////////////////////////////////////////////////////////////
+        private string dbID = "spring2022team5";
+        private string dbPass = "spring2022team5";
+        private string dbName = "spring2022team5";
+        ////////////////////////////////////////////////////////////////////////
 
-		/////////////////////////////////////////////////////////////////////////
-		//don't forget to include this decoration above each method that you want
-		//to be exposed as a web service!
-		[WebMethod(EnableSession = true)]
-		/////////////////////////////////////////////////////////////////////////
-		public string TestConnection()
-		{
-			try
-			{
-				string testQuery = "select * from employers";
+        ////////////////////////////////////////////////////////////////////////
+        ///call this method anywhere that you need the connection string!
+        ////////////////////////////////////////////////////////////////////////
+        private string getConString()
+        {
+            return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName + "; UID=" + dbID + "; PASSWORD=" + dbPass;
+        }
+        ////////////////////////////////////////////////////////////////////////
 
-				////////////////////////////////////////////////////////////////////////
-				///here's an example of using the getConString method!
-				////////////////////////////////////////////////////////////////////////
-				MySqlConnection con = new MySqlConnection(getConString());
-				////////////////////////////////////////////////////////////////////////
 
-				MySqlCommand cmd = new MySqlCommand(testQuery, con);
-				MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-				DataTable table = new DataTable() { TableName = "Employers" };
-				adapter.Fill(table);
+        /////////////////////////////////////////////////////////////////////////
+        //don't forget to include this decoration above each method that you want
+        //to be exposed as a web service!
+        [WebMethod(EnableSession = true)]
+        /////////////////////////////////////////////////////////////////////////
+        public string[] TestConnection()
+        {
+            try
+            {
+                string testQuery = "select * from employers";
 
-                
+                ////////////////////////////////////////////////////////////////////////
+                ///here's an example of using the getConString method!
+                ////////////////////////////////////////////////////////////////////////
+                MySqlConnection con = new MySqlConnection(getConString());
+                ////////////////////////////////////////////////////////////////////////
+
+                // connect and execute query 
+                MySqlCommand cmd = new MySqlCommand(testQuery, con);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                // create a data table and fill it with our data from query
+                DataTable table = new DataTable() { TableName = "Employers" };
+                adapter.Fill(table);
+
+                // creating a string array of users grabbing data from datatable rows
+                string[] users = new string[10];
+                users[0] = table.Rows[0].ItemArray[0].ToString();
+                users[1] = table.Rows[0].ItemArray[1].ToString();
+                users[2] = table.Rows[1].ItemArray[0].ToString();
+                users[3] = table.Rows[1].ItemArray[1].ToString();
+                users[4] = table.Rows[2].ItemArray[0].ToString();
+                users[5] = table.Rows[2].ItemArray[1].ToString();
+                users[6] = table.Rows[3].ItemArray[0].ToString();
+                users[7] = table.Rows[3].ItemArray[1].ToString();
+                users[8] = table.Rows[4].ItemArray[0].ToString();
+                users[9] = table.Rows[4].ItemArray[1].ToString();
+
+                // return the array
+                return users;
+
+                /* here are all of the columns and rows if need in the future for copy/paste
                 // columns
                 string useridcolumn = table.Columns[0].ColumnName.ToString();
                 string passwordcolumn = table.Columns[1].ColumnName.ToString();
@@ -143,74 +162,14 @@ namespace ProjectTemplate
                 string country5 = table.Rows[4].ItemArray[9].ToString();
                 string phone5 = table.Rows[4].ItemArray[10].ToString();
                 string website5 = table.Rows[4].ItemArray[11].ToString();
-
-                // return test query
-                return "Employer 1 \n" + useridcolumn + ": " + userid1 + "\n"
-                    + passwordcolumn + ": " + password1 + "\n"
-                    + firstnamecolumn + ": " + firstname1 + "\n"
-                    + lastnamecolumn + ": " + lastname1 + "\n"
-                    + companynamecolumn + ": " + companyname1 + "\n"
-                    + addresscolumn + ": " + address1 + "\n"
-                    + citycolumn + ": " + city1 + "\n"
-                    + statecolumn + ": " + state1 + "\n"
-                    + postalcodecolumn + ": " + postalcode1 + "\n"
-                    + countrycolumn + ": " + country1 + "\n"
-                    + phonecolumn + ": " + phone1 + "\n"
-                    + websitecolumn + ": " + website1 + "\n\n"
-                    + "Employer 2 \n" + useridcolumn + ": " + userid2 + "\n"
-                    + passwordcolumn + ": " + password2 + "\n"
-                    + firstnamecolumn + ": " + firstname2 + "\n"
-                    + lastnamecolumn + ": " + lastname2 + "\n"
-                    + companynamecolumn + ": " + companyname2 + "\n"
-                    + addresscolumn + ": " + address2 + "\n"
-                    + citycolumn + ": " + city2 + "\n"
-                    + statecolumn + ": " + state2 + "\n"
-                    + postalcodecolumn + ": " + postalcode2 + "\n"
-                    + countrycolumn + ": " + country2 + "\n"
-                    + phonecolumn + ": " + phone2 + "\n"
-                    + websitecolumn + ": " + website2 + "\n"
-                    + "Employer 3 \n" + useridcolumn + ": " + userid3 + "\n"
-                    + passwordcolumn + ": " + password3 + "\n"
-                    + firstnamecolumn + ": " + firstname3 + "\n"
-                    + lastnamecolumn + ": " + lastname3 + "\n"
-                    + companynamecolumn + ": " + companyname3 + "\n"
-                    + addresscolumn + ": " + address3 + "\n"
-                    + citycolumn + ": " + city3 + "\n"
-                    + statecolumn + ": " + state3 + "\n"
-                    + postalcodecolumn + ": " + postalcode3 + "\n"
-                    + countrycolumn + ": " + country3 + "\n"
-                    + phonecolumn + ": " + phone3 + "\n"
-                    + websitecolumn + ": " + website3 + "\n\n"
-                    + "Employer 4 \n" + useridcolumn + ": " + userid4 + "\n"
-                    + passwordcolumn + ": " + password4 + "\n"
-                    + firstnamecolumn + ": " + firstname4 + "\n"
-                    + lastnamecolumn + ": " + lastname4 + "\n"
-                    + companynamecolumn + ": " + companyname4 + "\n"
-                    + addresscolumn + ": " + address4 + "\n"
-                    + citycolumn + ": " + city4 + "\n"
-                    + statecolumn + ": " + state4 + "\n"
-                    + postalcodecolumn + ": " + postalcode4 + "\n"
-                    + countrycolumn + ": " + country4 + "\n"
-                    + phonecolumn + ": " + phone4 + "\n"
-                    + websitecolumn + ": " + website4 + "\n\n"
-                    + "Employer 5" + useridcolumn + ": " + userid5 + "\n"
-                    + passwordcolumn + ": " + password5 + "\n"
-                    + firstnamecolumn + ": " + firstname5 + "\n"
-                    + lastnamecolumn + ": " + lastname5 + "\n"
-                    + companynamecolumn + ": " + companyname5 + "\n"
-                    + addresscolumn + ": " + address5 + "\n"
-                    + citycolumn + ": " + city5 + "\n"
-                    + statecolumn + ": " + state5 + "\n"
-                    + postalcodecolumn + ": " + postalcode5 + "\n"
-                    + countrycolumn + ": " + country5 + "\n"
-                    + phonecolumn + ": " + phone5 + "\n"
-                    + websitecolumn + ": " + website5;
-
+                    */
             }
-			catch (Exception e)
-			{
-                return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
-			}
-		}
-	}
+            catch (Exception e)
+            {
+                string[] error = new string[1];
+                error[0] = e.Message;
+                return error;
+            }
+        }
+    }
 }
