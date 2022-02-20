ffd50f3ceae08f69aae6e394cd17d1a6ba5d0e6a
lindsk92 <94583966+lindsk92@users.noreply.github.com>
2022-02-19 22:31:29 -0700

    Updated ProjectServices

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -78,6 +78,7 @@ namespace ProjectTemplate
                 user.phone = table.Rows[i].ItemArray[10].ToString();
                 user.email = table.Rows[i].ItemArray[11].ToString();
                 user.website = table.Rows[i].ItemArray[12].ToString();
+                user.status = table.Rows[i].ItemArray[13].ToString();
 
                 // add to people list
                 people.Add(user);
@@ -107,11 +108,11 @@ namespace ProjectTemplate
                 con.Open();
 
 
-                // set up the command
+                // connect and execute query 
                 MySqlCommand cmd = new MySqlCommand(addUser, con);
 
 
-                // Pass values to Parameters
+                //Pass values to Parameters
                 cmd.Parameters.AddWithValue("@id", id);
                 cmd.Parameters.AddWithValue("@pass", pass);
                 cmd.Parameters.AddWithValue("@first", first);
@@ -126,8 +127,6 @@ namespace ProjectTemplate
                 cmd.Parameters.AddWithValue("@email", email);
                 cmd.Parameters.AddWithValue("@website", website);
                 cmd.Parameters.AddWithValue("@status", status);
-
-                // execute 
                 cmd.ExecuteNonQuery();
             }
             catch (Exception)
@@ -191,7 +190,6 @@ namespace ProjectTemplate
             // return an array of users 
             return unpeople.ToArray();
         }
-
 
         [WebMethod]
         public void ApproveUser(string id)
@@ -274,11 +272,11 @@ namespace ProjectTemplate
         }
 
         [WebMethod]
-        public void InsertResponse(int q1, string q2, int q3, string q4, int q5)
+        public void InsertResponse(int surveyid, int r1, string r2, int r3, string r4, int r5)
         {
             // insert statement
-            string insertResponse = "INSERT INTO responses (time, question1, question2, question3, question4, question5) " +
-                "VALUES (@time, @q1, @q2, @q3, @q4, @q5)";
+            string insertResponse = "INSERT INTO responses (surveyid, time, response1, response2, response3, response4, response5) " +
+                "VALUES (@surveyid, @time, @r1, @r2, @r3, @r4, @r5)";
 
             ////////////////////////////////////////////////////////////////////////
             ///here's an example of using the getConString method!
@@ -300,7 +298,94 @@ namespace ProjectTemplate
                 DateTime now = DateTime.Now;
 
                 //Pass values to Parameters
+                cmd.Parameters.AddWithValue("@surveyid", surveyid);
                 cmd.Parameters.AddWithValue("@time", now);
+                cmd.Parameters.AddWithValue("@r1", r1);
+                cmd.Parameters.AddWithValue("@r2", r2);
+                cmd.Parameters.AddWithValue("@r3", r3);
+                cmd.Parameters.AddWithValue("@r4", r4);
+                cmd.Parameters.AddWithValue("@r5", r5);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                // close connection
+                con.Close();
+            }
+            finally
+            {
+                // close connection
+                con.Close();
+            }
+        }
+
+
+        [WebMethod]
+        public Responses[] GetSurveyResults()
+        {
+            string query = "select * from responses";
+
+            ////////////////////////////////////////////////////////////////////////
+            ///here's an example of using the getConString method!
+            ////////////////////////////////////////////////////////////////////////
+            MySqlConnection con = new MySqlConnection(getConString());
+            ////////////////////////////////////////////////////////////////////////
+
+            // connect and execute query 
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+
+            // create a data table and fill it with our data from query
+            DataTable table = new DataTable() { TableName = "resultstable" };
+            adapter.Fill(table);
+
+            // create a list of survey results
+            List<Responses> results = new List<Responses>();
+
+            // loop to create a new user based on # of rows in data table
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                // create new user with data from datatable
+                Responses result = new Responses();
+                result.responseid = table.Rows[i].ItemArray[0].ToString();
+                result.surveyid = table.Rows[i].ItemArray[1].ToString();
+                result.time = table.Rows[i].ItemArray[2].ToString();
+                result.response1 = table.Rows[i].ItemArray[3].ToString();
+                result.response2 = table.Rows[i].ItemArray[4].ToString();
+                result.response3 = table.Rows[i].ItemArray[5].ToString();
+                result.response4 = table.Rows[i].ItemArray[6].ToString();
+                result.response5 = table.Rows[i].ItemArray[7].ToString();
+
+                // add to list of surveys
+                results.Add(result);
+            }
+            // return an array of users 
+            return results.ToArray();
+        }
+
+        [WebMethod]
+        public void InsertSurvey(string q1, string q2, string q3, string q4, string q5)
+        {
+            // insert statement
+            string insertResponse = "INSERT INTO surveys (question1, question2, question3, question4, question5) " +
+                "VALUES (@q1, @q2, @q3, @q4, @q5)";
+
+            ////////////////////////////////////////////////////////////////////////
+            ///here's an example of using the getConString method!
+            ////////////////////////////////////////////////////////////////////////
+            MySqlConnection con = new MySqlConnection(getConString());
+            ////////////////////////////////////////////////////////////////////////
+
+            try
+            {
+                // open connection
+                con.Open();
+
+
+                // connect and execute query 
+                MySqlCommand cmd = new MySqlCommand(insertResponse, con);
+
+                //Pass values to Parameters
                 cmd.Parameters.AddWithValue("@q1", q1);
                 cmd.Parameters.AddWithValue("@q2", q2);
                 cmd.Parameters.AddWithValue("@q3", q3);
@@ -319,5 +404,48 @@ namespace ProjectTemplate
                 con.Close();
             }
         }
+
+
+        [WebMethod]
+        public Surveys[] GetSurveyQuestions()
+        {
+            string query = "select * from surveys";
+
+            ////////////////////////////////////////////////////////////////////////
+            ///here's an example of using the getConString method!
+            ////////////////////////////////////////////////////////////////////////
+            MySqlConnection con = new MySqlConnection(getConString());
+            ////////////////////////////////////////////////////////////////////////
+
+            // connect and execute query 
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+
+            // create a data table and fill it with our data from query
+            DataTable table = new DataTable() { TableName = "surveystable" };
+            adapter.Fill(table);
+
+            // create a list of survey questions
+            List<Surveys> surveylist = new List<Surveys>();
+
+            // loop to create a new survey based on # of rows in data table
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                // create new survey with data from datatable
+                Surveys survey = new Surveys();
+                survey.surveyid = table.Rows[i].ItemArray[0].ToString();
+                survey.question1 = table.Rows[i].ItemArray[1].ToString();
+                survey.question2 = table.Rows[i].ItemArray[2].ToString();
+                survey.question3 = table.Rows[i].ItemArray[3].ToString();
+                survey.question4 = table.Rows[i].ItemArray[4].ToString();
+                survey.question5 = table.Rows[i].ItemArray[5].ToString();
+
+                // add to list of surveys
+                surveylist.Add(survey);
+            }
+            // return an array of surveys
+            return surveylist.ToArray();
+        }
+
     }
 }
---
+++ ProjectTemplate/Responses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Responses
+    {
+        public string responseid;
+        public string surveyid;
+        public string time;
+        public string response1;
+        public string response2;
+        public string response3;
+        public string response4;
+        public string response5;
+    }
+}
